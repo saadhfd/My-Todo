b94c4c9373bf4a5c299e33026c272f6f562f36fa
Saad@git <saad.hfd@gmail.com>
2024-03-25 20:59:00 +0200

    Part-1 done

---
+++ js/index.js
@@ -0,0 +1,131 @@
+const BACKEND_ROOT_URL = "https://todo-server-bh68.onrender.com";
+import { Todos } from "./class/Todos.js";
+
+const todos = new Todos(BACKEND_ROOT_URL);
+
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+input.disabled = true; //
+// console.log('testing');
+
+const renderTask = (task) => {
+  const li = document.createElement("li");
+  li.setAttribute("class", "list-group-item");
+  li.setAttribute("data-key", task.getId().toString());
+  renderSpan(li, task.getText()); // Render the text inside a span
+  renderLink(li, task.getId());
+  list.append(li);
+};
+
+const renderSpan = (li, text) => {
+  const span = document.createElement("span"); // Create a new span element
+  span.textContent = text; // Set the text content of the span
+  li.appendChild(span); // Append the span to the list item
+};
+
+const renderLink = (li, id) => {
+  const a = document.createElement("a");
+  a.innerHTML = '<i class="bi bi-trash"></i>';
+  a.setAttribute("style", "float: right");
+  a.addEventListener("click", (event) => {
+    todos
+      .removeTask(id)
+      .then((removed_id) => {
+        const li_to_remove = document.querySelector(
+          `[data-key = '${removed_id}']`
+        );
+        if (li_to_remove) {
+          list.removeChild(li_to_remove);
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  });
+  li.appendChild(a);
+};
+
+
+// const renderTask = (task) => {
+//   // console.log(task);
+//   const li = document.createElement('li');
+//   li.setAttribute('class', 'list-group-item');
+//   li.setAttribute('data-key', task.getId().toString());
+//   li.innerHTML = task.getText();
+//   renderSpan(li, task.getText());
+//   renderLink(li, task.getId());
+//   list.append(li);
+// };
+
+// const renderSpan = (li, text) => {
+//   const span = li.appendChild(document.createElement('span'));
+//   span.innerhtml = text;
+// };
+
+// const renderLink = (li, id) => {
+//   const a = li.appendChild(document.createElement('a'));
+//   a.innerhtml = '<i class="fa fa-trash"></i>';
+//   a.setAttribute('style','float: right');
+//   a.addEventListener("click", (event) => {
+//     todos
+//       .removeTask(id)
+//       .then((removed_id) => {
+//         const li_to_remove = document.querySelector(
+//           `[data-key = '${removed_id}']`
+//         );
+//         if (li_to_remove) {
+//           list.removeChild(li_to_remove);
+//         }
+//       })
+//       .catch((error) => {
+//         alert(error);
+//       });
+//   });
+// };
+const getTasks = () => {
+  todos
+    .getTasks()
+    .then((tasks) => {
+      tasks.forEach((task) => {
+        renderTask(task);
+      });
+      input.disabled = false;
+    })
+    .catch((error) => {
+      alert(error);
+    });
+};
+
+const saveTask = async (task) => {
+  try {
+    const json = JSON.stringify({ description: task });
+    const response = await fetch(BACKEND_ROOT_URL + "/new", {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: json,
+    });
+    return response.json();
+  } catch (error) {
+    alert("Error saving task " + error.message);
+  }
+};
+
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    const task = input.value.trim();
+    if (task !== " ") {
+      todos.addTask(task).then((task) => {
+        renderTask(task);
+        input.value = "";
+        input.focus();
+      });
+    } else {
+      return Error;
+    }
+  }
+});
+getTasks();
+// saveTask();/
